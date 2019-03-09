eaef08c2b24df18c76b393e4d84985baf739e6ea
kam-ahuja <kamahuja@gmail.com>
2019-03-08 18:17:07 -0800

    Add project files.

---
+++ okta-heartbeat/Function1.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Okta.Core;
+using Okta.Core.Clients;
+
+namespace oktaheartbeat
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static void Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, TraceWriter log)
+        {
+            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            var tenant = "https://org.oktapreview.com";
+            var username = "john.doe";
+            var password = "Hello#World!";
+
+
+            var oktaClient = new OktaClient("", new Uri(tenant));
+            var authClient = oktaClient.GetAuthClient();
+            var response = authClient.Authenticate(username, password);
+
+            log.Info(response.Status);
+        }
+    }
+}
